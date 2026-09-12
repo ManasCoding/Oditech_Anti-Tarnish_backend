463f175fef0e3ff3a75ef65441a99fda652cc0dc
Manas Kumar Gumansingh <gumansingh.dev.2000@gmail.com>
2026-09-12 21:57:55 +0530

    new changes

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://oditech-anti-tarnish-frontend.vercel.app',
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
